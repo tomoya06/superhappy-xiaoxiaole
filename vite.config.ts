0b112d5f9b498c56b23cf89999f52a9304993d25
ITIStomoya06 <35499042+tomoya06@users.noreply.github.com>
2022-02-14 01:19:35 +0800

    gh-pages部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import preact from "@preact/preset-vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: '/superhappy-xiaoxiaole/',
   server: {
     host: "0.0.0.0",
   },
