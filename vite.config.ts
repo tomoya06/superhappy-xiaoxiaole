69425aa7444832c00593052bfbe0c55596f77f9c
ITIStomoya06 <35499042+tomoya06@users.noreply.github.com>
2022-10-08 16:24:24 +0800

    feat: script

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-  base: '/superhappy-xiaoxiaole/',
-  server: {
-    host: "0.0.0.0",
-  },
+export default (({ mode }) => {
+  const base = mode === 'github' ? '/superhappy-xiaoxiaole/' : '';
+
+  return defineConfig({
+    plugins: [preact()],
+    base,
+    server: {
+      host: "0.0.0.0",
+    },
+  })
 });
